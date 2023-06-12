e7af49ef2a2b6db799eab71715ec7b6e78f87905
Max <sultonbek.rakhimov@gmail.com>
2023-06-12 13:38:00 +0500

    Created home task definitions

---
+++ src/LoopingHomeTask/HomeTask.cs
@@ -0,0 +1,72 @@
+﻿namespace LoopingHomeTask;
+
+public enum ContentQuality
+{
+    Low,
+    Normal,
+    Good
+}
+
+public enum RockPaperScissor
+{
+    Rock,
+    Paper,
+    Scissor
+}
+
+public enum Player
+{
+    PlayerA,
+    PlayerB
+}
+
+public class HomeTask
+{
+    #region Processing Collections
+
+    // Task A 
+    public static string FindMostUsedWord(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Task B 
+    public static ContentQuality CalculateQualityOfText(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #region Indefinite Operations
+
+    // Task C
+    public static int GuessTheNumber()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Task D 
+    public static int ValidateAge()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #region Iterative Processing
+
+    // Task E : 
+    public static int CalculateFibonacci(int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Task F : 
+    public static void RockPaperScissorsGame()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
