5a372b358cde3888280808daffb4078226fac85f
Max <sultonbek.rakhimov@gmail.com>
2023-06-19 18:54:01 +0500

    Created library management model

---
+++ src/LibraryManagement/LibraryManagement.cs
@@ -0,0 +1,26 @@
+﻿namespace LibraryManagement
+{
+    public class LibraryManagement : LibraryManagementCore
+    {
+        public void RegisterUser(string firstName, string lastName)
+        {
+
+        }
+
+        public void RegisterUser(string firstName, string lastName, string userName)
+        {
+            var foundUser = false;
+            foreach (var user in Store.Users)
+                if (user.UserName == userName)
+                {
+                    foundUser = true;
+                    break;
+                }
+
+            if (foundUser)
+                return;
+
+            Store.Users.Add(new User(firstName, lastName, userName));
+        }
+    }
+}
