f097bdb28e2ca570bc911133133605b6feaba10c
Max <sultonbek.rakhimov@gmail.com>
2023-06-20 19:47:18 +0500

    Add email sending functionality to library management

--- src/LibraryManagement/LibraryManagement.cs
+++ src/LibraryManagement/LibraryManagement.cs
@@ -1,17 +1,24 @@
-﻿namespace LibraryManagement
+﻿namespace Library
 {
     public class LibraryManagement : LibraryManagementCore
     {
+        private EmailService emailService;
+
+        public LibraryManagement()
+        {
+            emailService = new EmailService();
+        }
+
         public void RegisterUser(string firstName, string lastName)
         {
-
+            
         }
 
         public void RegisterUser(string firstName, string lastName, string userName)
         {
             var foundUser = false;
             foreach (var user in Store.Users)
-                if (user.UserName == userName)
+                if (user.Email == userName)
                 {
                     foundUser = true;
                     break;
@@ -21,6 +28,7 @@
                 return;
 
             Store.Users.Add(new User(firstName, lastName, userName));
+            emailService.SendEmail(userName);
         }
     }
 }
